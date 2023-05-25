152292616e2465601a268a11a31b6a3727a65c52
Addr678 <128295657+Addr678@users.noreply.github.com>
2023-05-25 09:54:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,82 +1,106 @@
 //your code here
-document.addEventListener("DOMContentLoaded", () => {
-  const gameContainer = document.getElementById("gameContainer");
-  const gridSize = 10;
-  const gridCount = 400 / gridSize;
-  let food;
-  let snake;
-  let direction;
-  let interval;
+  document.addEventListener('DOMContentLoaded', () => {
+  const gameContainer = document.getElementById('gameContainer');
+  const scoreElement = document.getElementById('score');
 
-  function createPixel(id, className) {
-    const pixel = document.createElement("div");
-    pixel.id = id;
-    pixel.classList.add(className);
-    return pixel;
-  }
+  const gridSize = 10;
+  const gridWidth = gameContainer.offsetWidth / gridSize;
+  const gridHeight = gameContainer.offsetHeight / gridSize;
+
+  let snake = [{ x: 10, y: 20 }];
+  let food = createFood();
+  let direction = 'right';
+  let score = 0;
+
+  const moveInterval = setInterval(moveSnake, 100);
+
+  document.addEventListener('keydown', changeDirection);
 
   function createFood() {
-    const x = Math.floor(Math.random() * gridCount);
-    const y = Math.floor(Math.random() * gridCount);
-    const foodId = `food-${x}-${y}`;
-    food = createPixel(foodId, "food");
-    food.style.left = `${x * gridSize}px`;
-    food.style.top = `${y * gridSize}px`;
+    const food = document.createElement('div');
+    food.className = 'pixel food';
+    food.id = 'food';
     gameContainer.appendChild(food);
+
+    const foodX = Math.floor(Math.random() * gridWidth);
+    const foodY = Math.floor(Math.random() * gridHeight);
+    food.style.left = `${foodX * gridSize}px`;
+    food.style.top = `${foodY * gridSize}px`;
+
+    return { x: foodX, y: foodY };
   }
 
-  function createSnake() {
-    snake = [
-      { x: 5, y: 5 },
-      { x: 4, y: 5 },
-      { x: 3, y: 5 },
-    ];
+  function moveSnake() {
+    const head = { ...snake[0] };
+
+    if (direction === 'right') head.x += 1;
+    if (direction === 'left') head.x -= 1;
+    if (direction === 'up') head.y -= 1;
+    if (direction === 'down') head.y += 1;
+
+    snake.unshift(head);
+
+    if (head.x === food.x && head.y === food.y) {
+      score += 1;
+      scoreElement.textContent = score;
+
+      food.remove();
+      food = createFood();
+    } else {
+      snake.pop();
+    }
+
+    if (isCollision()) {
+      clearInterval(moveInterval);
+      alert('Game Over! Your score: ' + score);
+    }
+
+    renderSnake();
+  }
+
+  function renderSnake() {
+    const snakePixels = document.getElementsByClassName('snakeBodyPixel');
+    while (snakePixels.length > 0) {
+      snakePixels[0].remove();
+    }
 
     for (let i = 0; i < snake.length; i++) {
-      const pixelId = `pixel-${snake[i].x}-${snake[i].y}`;
-      const snakePixel = createPixel(pixelId, "snakeBodyPixel");
+      const snakePixel = document.createElement('div');
+      snakePixel.className = 'pixel snakeBodyPixel';
       snakePixel.style.left = `${snake[i].x * gridSize}px`;
       snakePixel.style.top = `${snake[i].y * gridSize}px`;
       gameContainer.appendChild(snakePixel);
     }
   }
 
-  function moveSnake() {
-    const head = { x: snake[0].x, y: snake[0].y };
-
-    switch (direction) {
-      case "up":
-        head.y -= 1;
-        break;
-      case "down":
-        head.y += 1;
-        break;
-      case "left":
-        head.x -= 1;
-        break;
-      case "right":
-        head.x += 1;
-        break;
-    }
-
-    const newHeadId = `pixel-${head.x}-${head.y}`;
-    const newHead = createPixel(newHeadId, "snakeBodyPixel");
-    newHead.style.left = `${head.x * gridSize}px`;
-    newHead.style.top = `${head.y * gridSize}px`;
-
-    snake.unshift(head);
-    gameContainer.appendChild(newHead);
-
-    if (head.x === food.offsetLeft / gridSize && head.y === food.offsetTop / gridSize) {
-      gameContainer.removeChild(food);
-      createFood();
-    } else {
-      const tail = snake.pop();
-      const tailId = `pixel-${tail.x}-${tail.y}`;
-      const tailPixel = document.getElementById(tailId);
-      gameContainer.removeChild(tailPixel);
-    }
+  function changeDirection(event) {
+    const key = event.keyCode;
+    if (key === 37 && direction !== 'right') direction = 'left';
+    if (key === 38 && direction !== 'down') direction = 'up';
+    if (key === 39 && direction !== 'left') direction = 'right';
+    if (key === 40 && direction !== 'up') direction = 'down';
   }
 
-  function handleKeyPress(event) {
-    const
+  function isCollision() {
+    const head = snake[0];
+
+    if (
+      head.x < 0 ||
+      head.x >= gridWidth ||
+      head.y < 0 ||
+      head.y >= gridHeight
+    ) {
+      return true;
+    }
+
+    for (let i = 1; i < snake.length; i++) {
+      if (head.x === snake[i].x && head.y === snake[i].y) {
+        return true;
+      }
+    }
+
+    return false;
+  }
+});
+
+ 
